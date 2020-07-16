86a4a8f24a5bd5698622acf2290d47c4363aceae
TheoChow <hoifung1024@hotmail.com>
2020-07-16 17:10:37 +0100

    Dennis Comments

--- Test5/Assets/Scripts/Task 1/Task1.cs
+++ Test5/Assets/Scripts/Task 1/Task1.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Task1 : MonoBehaviour
 {
     public Sprite triangle;
-    public Sprite diamond;
+    public Sprite diamond; 
+    
+    //Can possible do [SerializeField] private Sprite[] shapes
+    //This makes it so you can have an array of different shapes (say 10) to choose from, could be useful for larger scale
+    //Private to make it inaccessable by other scripts and don't show up on inspector, great to keep coding 'safe' but not important in this case
+    //I like to use private variables whereever posible so its enater.
+    //Note: public variables show up in inspector unless you do [NonSerialized]/[HideInInspector] which makes inspector "cleaner" 
 
     private SpriteRenderer spriteR;
 
@@ -28,5 +34,6 @@ public class Task1 : MonoBehaviour
         }
 
     }
-
+//This is good! works perfectly. Although the else statement will never be ran, therefore unnecessary. Can think about how to change from one image to another
+//but works fine :) Well Done! 
 }
